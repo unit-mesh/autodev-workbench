949aa98128b026050d1aebb1c96d4de2b02ce740
Phodal Huang <h@phodal.com>
2025-05-08 21:01:43 +0800

    feat: add rollup-plugin-copy for asset management and update main.ts for language service instantiation

--- packages/work/rollup.config.js
+++ packages/work/rollup.config.js
@@ -3,6 +3,7 @@ const typescript = require("@rollup/plugin-typescript").default;
 const { nodeResolve } = require("@rollup/plugin-node-resolve");
 const commonjs = require("@rollup/plugin-commonjs").default;
 const json = require("@rollup/plugin-json").default;
+const copy = require("rollup-plugin-copy");
 
 module.exports = defineConfig({
   input: "src/main.ts",
@@ -24,6 +25,22 @@ module.exports = defineConfig({
       tsconfig: "tsconfig.json",
       importHelpers: true,
       sourceMap: false,
+    }),
+    copy({
+      targets: [
+        {
+          src: 'node_modules/@unit-mesh/treesitter-artifacts/wasm/*.wasm',
+          dest: 'bin/tree-sitter-wasms',
+        },
+        {
+          src: 'node_modules/web-tree-sitter/*.wasm',
+          dest: 'bin',
+        },
+        {
+          src: 'src/code-search/schemas/indexes/*.scm',
+          dest: 'bin/semantic',
+        },
+      ],
     }),
   ],
 });
